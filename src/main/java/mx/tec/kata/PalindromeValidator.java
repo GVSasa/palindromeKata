cf5d5b699b2e4d3c9f43f810fd7248856243a595
GVSasa <Sebastiancitodelasierra@hotmail.com>
2021-04-16 17:07:13 -0600

    Challenge 2

--- src/main/java/mx/tec/kata/PalindromeValidator.java
+++ src/main/java/mx/tec/kata/PalindromeValidator.java
@@ -1,15 +1,20 @@
 package mx.tec.kata;
 
 public class PalindromeValidator {
-
 	
 	public boolean validate(String string) {
 		if(string.length() == 1) {
 			return true;
-		}else {
+			
+		}
+		else if(string.length() == 2 && string.charAt(0) == string.charAt(1)){
+			
+			return true;			
+		}
+		else {
 			return false;
 		}
-		
+			
 	}
-	
+
 }
--- src/test/java/mx/tec/kata/PalindromeValidatorTest.java
+++ src/test/java/mx/tec/kata/PalindromeValidatorTest.java
@@ -23,4 +23,32 @@ class PalindromeValidatorTest {
 		
 	}
 
+	@Test
+	void Given_2DifferentLettersString_When_ValidatingPalindrome_Then_False() {
+		// Given
+		String string = "ab";
+		
+		// When
+		boolean actualResult = validator.validate(string);
+		
+		// Then
+		boolean expectedResult = false;
+		assertEquals(expectedResult, actualResult);
+		
+	}
+	
+	@Test
+	void Given_2EqualLettersString_When_ValidatingPalindrome_Then_True() {
+		// Given
+		String string = "aa";
+		
+		// When
+		boolean actualResult = validator.validate(string);
+		
+		// Then
+		boolean expectedResult = true;
+		assertEquals(expectedResult, actualResult);
+		
+	}
+
 }
